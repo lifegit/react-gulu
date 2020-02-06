d13f451f7acd6b79270c060243ba084812fc1475
y <597985317@qq.com>
2020-02-07 04:27:41 +0800

    push

--- antdMobile/components/CarouselSimple/Index.js
+++ antdMobile/components/CarouselSimple/Index.js
@@ -1,57 +1,46 @@
-import React from "react";
+import React from 'react';
 import { Carousel } from 'antd-mobile';
 
-export default class Index extends React.Component {
+export default class Index extends React.PureComponent {
     state = {
-        imgHeight: 300,
+        imgHeight: 170,
     }
 
 
     render() {
-
         const { data = [ ] } = this.props;
-
         return(
-            <Carousel
+          (
+            data.length ? (
+              <Carousel
                 autoplay={true}
                 autoplayInterval={5000}
                 infinite
                 // beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
                 // afterChange={index => console.log('slide to', index)}
-            >
-                {/*{this.state.data.map(val => (*/}
-                {/*    <a*/}
-                {/*        key={val}*/}
-                {/*        href="http://www.alipay.com"*/}
-                {/*        style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}*/}
-                {/*    >*/}
-                {/*        <img*/}
-                {/*            src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}*/}
-                {/*            alt=""*/}
-                {/*            style={{ width: '100%', verticalAlign: 'top' }}*/}
-                {/*            onLoad={() => {*/}
-                {/*                // fire window resize event to change height*/}
-                {/*                window.dispatchEvent(new Event('resize'));*/}
-                {/*                this.setState({ imgHeight: 'auto' });*/}
-                {/*            }}*/}
-                {/*        />*/}
-                {/*    </a>*/}
-                {/*))}*/}
-
-                {data.map((val,key) => (
+              >
+                {data.map((item,key) => (
+                  <a
+                    target='_blank'
+                    key={key}
+                    href={item.href}
+                    style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                  >
                     <img
-                        key={key}
-                        src={val}
-                        alt=""
-                        style={{ width: '100%', verticalAlign: 'top' }}
-                        onLoad={() => {
-                            // fire window resize event to change height
-                            window.dispatchEvent(new Event('resize'));
-                            this.setState({ imgHeight: 'auto' });
-                        }}
+                      src={item.img}
+                      alt=""
+                      style={{ width: '100%', verticalAlign: 'top' }}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                      }}
                     />
+                  </a>
                 ))}
-            </Carousel>
+              </Carousel>
+            ): null
+          )
         )
     }
 }
